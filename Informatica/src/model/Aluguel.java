f3e91963364f157de4db8f10caec1bbde8c78e04
cunhaluisg5 <cunhaluisg5@gmail.com>
2018-04-13 11:06:06 -0300

    Criada classe Aluguel

---
+++ Informatica/src/model/Aluguel.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Luís Gustavo
+ */
+public class Aluguel {
+    private Cliente cliente;
+    private Equipamento equipamento;
+
+    public Aluguel() {
+    }
+
+    public Aluguel(Cliente cliente, Equipamento equipamento) {
+        this.cliente = cliente;
+        this.equipamento = equipamento;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Equipamento getEquipamento() {
+        return equipamento;
+    }
+
+    public void setEquipamento(Equipamento equipamento) {
+        this.equipamento = equipamento;
+    }
+}
