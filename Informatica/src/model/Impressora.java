cb0d643ac7ff32d14e572af8bce581b5bba059f1
cunhaluisg5 <cunhaluisg5@gmail.com>
2018-04-15 17:32:36 -0300

    Criada classe Impressora

---
+++ Informatica/src/model/Impressora.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Luís Gustavo
+ */
+public class Impressora implements Equipamento{
+    private String tipo;
+    private String tecnologia;
+    private String especificacao;
+
+    public Impressora() {
+    }
+
+    public Impressora(String tipo, String tecnologia, String especificacao) {
+        this.tipo = tipo;
+        this.tecnologia = tecnologia;
+        this.especificacao = especificacao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTecnologia() {
+        return tecnologia;
+    }
+
+    public void setTecnologia(String tecnologia) {
+        this.tecnologia = tecnologia;
+    }
+
+    public String getEspecificacao() {
+        return especificacao;
+    }
+
+    public void setEspecificacao(String especificacao) {
+        this.especificacao = especificacao;
+    }
+
+    @Override
+    public String codEquipamento() {
+        return "";
+    }
+
+    @Override
+    public int quantidade() {
+        return 0;
+    }
+}
