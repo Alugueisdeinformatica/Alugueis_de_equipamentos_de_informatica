16ba47f7a330996d97e03177790aec362f584c14
joaopresto <jguedespresto@gmail.com>
2018-06-13 20:59:45 -0300

    Altera de TextArea para Tabela no BuscarCliente

--- Informatica/src/form/FormBuscarCliente.java
+++ Informatica/src/form/FormBuscarCliente.java
@@ -3,12 +3,14 @@ package form;
 import java.util.List;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 import model.Cliente;
 
 public class FormBuscarCliente extends javax.swing.JFrame {
 
     private static Cliente clien = null;
     public static int seletor;
+    DefaultTableModel modeloCliente  = null;
     
     public static void setCliente(Cliente clien){
         FormBuscarCliente.clien = clien;
@@ -20,6 +22,7 @@ public class FormBuscarCliente extends javax.swing.JFrame {
     
     public FormBuscarCliente() {
         initComponents();
+        this.modeloCliente = (DefaultTableModel) tbClientes.getModel();
     }
     
     public static boolean janela = false;
@@ -33,11 +36,11 @@ public class FormBuscarCliente extends javax.swing.JFrame {
         tfCPF = new javax.swing.JFormattedTextField();
         cboBuscarTodos = new javax.swing.JCheckBox();
         btBuscar = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        taBusca = new javax.swing.JTextArea();
         btSair = new javax.swing.JButton();
         btSelecionar = new javax.swing.JButton();
         btEditar = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        tbClientes = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Consulta de Cliente");
@@ -81,14 +84,6 @@ public class FormBuscarCliente extends javax.swing.JFrame {
             }
         });
 
-        taBusca.setEditable(false);
-        taBusca.setColumns(20);
-        taBusca.setLineWrap(true);
-        taBusca.setRows(5);
-        taBusca.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.LOWERED));
-        taBusca.setName("taBusca"); // NOI18N
-        jScrollPane1.setViewportView(taBusca);
-
         btSair.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         btSair.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/Apps-Dialog-Close-icon.png"))); // NOI18N
         btSair.setText("Sair");
@@ -119,32 +114,73 @@ public class FormBuscarCliente extends javax.swing.JFrame {
             }
         });
 
+        tbClientes.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Nome", "CPF", "Endereço", "E-mail", "Telefone"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        tbClientes.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                tbClientesMousePressed(evt);
+            }
+        });
+        jScrollPane2.setViewportView(tbClientes);
+        if (tbClientes.getColumnModel().getColumnCount() > 0) {
+            tbClientes.getColumnModel().getColumn(0).setResizable(false);
+            tbClientes.getColumnModel().getColumn(1).setResizable(false);
+            tbClientes.getColumnModel().getColumn(2).setResizable(false);
+            tbClientes.getColumnModel().getColumn(3).setResizable(false);
+            tbClientes.getColumnModel().getColumn(4).setResizable(false);
+        }
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(51, 51, 51)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGap(34, 34, 34)
+                .addComponent(btEditar)
+                .addGap(89, 89, 89)
+                .addComponent(btSelecionar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btSair)
+                .addGap(47, 47, 47))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(46, 46, 46)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(34, 34, 34)
-                                .addComponent(lbCPF))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(tfCPF, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(74, 74, 74)
-                                .addComponent(cboBuscarTodos)))
+                        .addGap(34, 34, 34)
+                        .addComponent(lbCPF)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(tfCPF, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btBuscar))
-                    .addComponent(jScrollPane1)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(btEditar)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 63, Short.MAX_VALUE)
-                        .addComponent(btSelecionar)
-                        .addGap(59, 59, 59)
-                        .addComponent(btSair)))
-                .addGap(70, 70, 70))
+                        .addComponent(cboBuscarTodos)
+                        .addGap(79, 79, 79)))
+                .addComponent(btBuscar)
+                .addGap(63, 63, 63))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 566, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(23, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -154,22 +190,23 @@ public class FormBuscarCliente extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(lbCPF)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(tfCPF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(btBuscar)
-                    .addComponent(cboBuscarTodos))
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 203, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(tfCPF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(cboBuscarTodos)))
+                    .addComponent(btBuscar))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(35, 35, 35)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btEditar)
-                    .addComponent(btSair, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(btSelecionar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(34, 34, 34))
+                    .addComponent(btSelecionar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btSair, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(24, 24, 24))
         );
 
         getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
 
-        setSize(new java.awt.Dimension(626, 443));
+        setSize(new java.awt.Dimension(626, 391));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
@@ -187,32 +224,25 @@ public class FormBuscarCliente extends javax.swing.JFrame {
     }//GEN-LAST:event_btSairActionPerformed
 
     private void btBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btBuscarActionPerformed
-        btEditar.setEnabled(false);
-        taBusca.setText("");
+        btEditar.setEnabled(false); 
+        limparTabela();
         if(cboBuscarTodos.isSelected()){
             List<Cliente> cl = FormPrincipal.bdCliente.todosClientes();
-            if(cl.size() > 0){
+            if(cl.size() > 0){               
                 for(int i = 0; i < cl.size(); i++){
-                    taBusca.append(cl.get(i).toString() + cl.get(i).getEndereco().toString() + "\n");                    
+                    inserirTabelaCliente(cl, i);                    
                 }
             }else{
                 JOptionPane.showMessageDialog(null, "Não existem clientes cadastrados!", "Informação de Cadastro", JOptionPane.INFORMATION_MESSAGE);
             }
         }else{
-            String cpsSemMascara = tfCPF.getText().replace(".", "").replace("-", "");
+            String cpf = tfCPF.getText();
+            String cpsSemMascara = cpf.replace(".", "").replace("-", "");
             if(FormCliente.validarCPF(cpsSemMascara)){
                 if(!cpsSemMascara.trim().equals("")){
-                    Cliente cliente = FormPrincipal.bdCliente.buscaCliente(tfCPF.getText());
+                    Cliente cliente = FormPrincipal.bdCliente.buscaCliente(cpf);
                     if(cliente != null){
-                        taBusca.setText(cliente.toString() + cliente.getEndereco().toString() + "\n");
-                        setCliente(cliente);
-                        if(seletor == 0){
-                            btEditar.setEnabled(true);
-                            btSelecionar.setEnabled(false);
-                        }else{
-                            btEditar.setEnabled(false);
-                            btSelecionar.setEnabled(true);
-                        }                        
+                        inserirTabelaCliente(cliente);                                                                       
                     }else{
                         JOptionPane.showMessageDialog(null, "Cliente não cadastrado!", "Informação de Cadastro", JOptionPane.INFORMATION_MESSAGE);
                     }
@@ -223,6 +253,34 @@ public class FormBuscarCliente extends javax.swing.JFrame {
             }
         }
     }//GEN-LAST:event_btBuscarActionPerformed
+
+    private void inserirTabelaCliente(Cliente cli) {
+        modeloCliente.addRow(new Object[]{
+            cli.getNome(), 
+            cli.getCpf(), 
+            cli.getEndereco().getRua() + ", " 
+                    + cli.getEndereco().getBairro() + " ," + cli.getEndereco().getCidade(), 
+            cli.getEmail(), 
+            cli.getTelefone()
+        });
+    }
+
+    private void inserirTabelaCliente(List<Cliente> cl, int i) {
+        modeloCliente.addRow(new Object[] {
+            cl.get(i).getNome(),
+            cl.get(i).getCpf(),
+            cl.get(i).getEndereco().getRua() +", "+ cl.get(i).getEndereco().getBairro()+
+                    " ,"+cl.get(i).getEndereco().getCidade(),
+            cl.get(i).getEmail(),
+            cl.get(i).getTelefone()
+        });
+    }
+    
+    private void limparTabela() {
+        for (int i = tbClientes.getRowCount() - 1; i >= 0; --i){
+            modeloCliente.removeRow(i);
+        }
+    }
 
     private void btSelecionarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btSelecionarActionPerformed
         if(janela == true){
@@ -237,7 +295,7 @@ public class FormBuscarCliente extends javax.swing.JFrame {
     }//GEN-LAST:event_btSelecionarActionPerformed
 
     private void btEditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btEditarActionPerformed
-        FormCliente.seletor = 1;
+        FormCliente.seletor = 1;        
         new FormCliente().setVisible(true);
         FormCliente.setCliente(getCliente());
         this.dispose();
@@ -252,6 +310,19 @@ public class FormBuscarCliente extends javax.swing.JFrame {
             cboBuscarTodos.setEnabled(false);
         }
     }//GEN-LAST:event_formWindowOpened
+
+    private void tbClientesMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tbClientesMousePressed
+        String cpf = (String) (modeloCliente.getValueAt(tbClientes.getSelectedRow(), 1));        
+        Cliente cliente = FormPrincipal.bdCliente.buscaCliente(cpf);         
+        setCliente(cliente);
+        if(seletor == 0){
+            btEditar.setEnabled(true);
+            btSelecionar.setEnabled(false);
+        }else{
+            btEditar.setEnabled(false);
+            btSelecionar.setEnabled(true);
+        }
+    }//GEN-LAST:event_tbClientesMousePressed
 
     /**
      * @param args the command line arguments
@@ -295,9 +366,9 @@ public class FormBuscarCliente extends javax.swing.JFrame {
     private javax.swing.JButton btSelecionar;
     private javax.swing.JCheckBox cboBuscarTodos;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel lbCPF;
-    private javax.swing.JTextArea taBusca;
+    private javax.swing.JTable tbClientes;
     private javax.swing.JFormattedTextField tfCPF;
     // End of variables declaration//GEN-END:variables
 }
